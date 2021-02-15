19f3ee115e670555428f4d85b56236e9abc87409
DaeHoon <32921115+daehoon12@users.noreply.github.com>
2021-02-15 14:24:17 +0900

    Update emnist_cnn.py

--- Model/emnist_cnn.py
+++ Model/emnist_cnn.py
@@ -84,9 +84,9 @@ model = Sequential()
 
 model = tf.keras.models.Sequential()
 
-model.add(tf.keras.layers.Conv2D(64, (3,3), activation='relu',padding ='same', input_shape=(28,28,1)))
+model.add(tf.keras.layers.Conv2D(32, (3,3), activation='relu',padding ='same', input_shape=(28,28,1)))
 model.add(tf.keras.layers.MaxPooling2D(2,2))
-model.add(tf.keras.layers.Conv2D(32, (3,3), activation='relu',padding ='same',))
+model.add(tf.keras.layers.Conv2D(64, (3,3), activation='relu',padding ='same',))
 model.add(tf.keras.layers.MaxPooling2D(2,2))
 model.add(Dropout(0.2))
 model.add(tf.keras.layers.Flatten())
